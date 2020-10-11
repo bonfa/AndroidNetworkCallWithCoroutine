b6fee441b1f1e4563d8f66cdecc3c706d5f60ae6
francesco.bonfadelli <francesco.bonfadelli@gmail.com>
2020-10-11 12:03:35 +0200

    create new app

---
+++ app/src/main/java/it/fbonfadelli/coroutinetrial/WeatherActivity.kt
@@ -0,0 +1,11 @@
+package it.fbonfadelli.coroutinetrial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
